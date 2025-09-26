eb05283b0d0217eeab89be8899f237f4a7b5f427
kathiravanr15 <kathiravanr1520@gmail.com>
2025-09-27 02:08:06 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/Csv-book-editor/", 
+  base: "./", // <-- use relative paths for root deployment
   plugins: [
     react({
       babel: {
